e6206efd278d428f9f071318eeaeec083e349ef6
Wilmer H. Muñoz <wilmerhmg@gmail.com>
2018-05-11 14:02:16 -0500

    Enigma - Ajax

--- test/validador.php
+++ test/validador.php
@@ -3,13 +3,20 @@ require_once '../php-api/enigma.api.php';
 $Enigma = new Enigma();
 $Enigma->go();
 
-$option = isset($_POST['option']) ? $_POST['option'] : FALSE;
-$nombres = isset($_POST['nombres']) ? $_POST['nombres'] : FALSE;
-$email = isset($_POST['email']) ? $_POST['email'] : FALSE;
-$card_number = isset($_POST['card_number']) ? $_POST['card_number'] : FALSE;
+$option    = isset($_POST['servicios']) ? $_POST['servicios'] : FALSE;
+$nombres   = isset($_POST['nombres']) ? $_POST['nombres'] : FALSE;
+$email     = isset($_POST['email']) ? $_POST['email'] : FALSE;
+$codigo    = isset($_POST['codigo']) ? $_POST['codigo'] : FALSE;
 $card_date = isset($_POST['card_date']) ? $_POST['card_date'] : FALSE;
-$card_cvs = isset($_POST['card_cvs']) ? $_POST['card_cvs'] : FALSE;
+$card_cvs  = isset($_POST['card_cvs']) ? $_POST['card_cvs'] : FALSE;
 
-$informacion = array('info'=>'Pago suscripcion realizada','codigo'=>'7CV334VB5CC2DDD34','usuario'=>'Carlos','clave'=>'tomates2017');
+$informacion = array(
+	'info'       => 'Pago suscripcion realizada',
+	'referencia' => '7CV334VB5CC2DDD34',
+	'codigo'     => $codigo,
+	'servicios'  => $option
+);
 
+
+//Usa el metodo encrypt de la clase Enigma para retornar valores cifrados desde el servidor hacia el cliente
 echo $Enigma->encrypt(json_encode($informacion));
